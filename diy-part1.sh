4afcb40b7f657c916f0534adf9303eaa8baab629
yvzz <15711009+yvzz@users.noreply.github.com>
2026-06-30 22:49:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,15 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+mkdir -p package
+Copy custom local packages into OpenWrt tree so they are available during build
+if [ -d "$GITHUB_WORKSPACE/package/luci-compat-keep" ]; then
+  cp -r "$GITHUB_WORKSPACE/package/luci-compat-keep" package/
+fi
+
+git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
